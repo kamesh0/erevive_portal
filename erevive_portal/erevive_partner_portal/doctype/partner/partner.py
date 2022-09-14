c50bebe42e0b674aa5fa203f3b6d95cbe07f6506
frappe-sonuarya <frappe-sonuarya@DESKTOP-TLNGFI8.localdomain>
2022-09-14 13:30:53 +0530

    Start

--- erevive_portal/erevive_partner_portal/doctype/partner/partner.py
+++ erevive_portal/erevive_partner_portal/doctype/partner/partner.py
@@ -1,8 +1,17 @@
 # Copyright (c) 2022, eRevive and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Partner(Document):
-	pass
+	def after_insert(self):
+		try:
+			frappe.sendmail(
+				recipients=self.email_id,
+				template="partner_signup",
+				args={"first_name":self.primary_contact_person},
+				header=
+			)
+		except Exception as e:
+			frappe.throw(e)
