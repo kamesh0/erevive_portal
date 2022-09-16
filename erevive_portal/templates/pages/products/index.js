f88b27b40e7dfa63e96f9c1e853d85dda35345ae
kamesh0 <kamesh@erevive.in>
2022-09-16 14:19:16 +0530

    search filter added in product page on product type Tally addons #10 #7

--- erevive_portal/templates/pages/products/index.js
+++ erevive_portal/templates/pages/products/index.js
@@ -1,18 +1,32 @@
-// frappe.ready(function(){
-//     $("#filter").keyup(function() {
-//         var filter = $("#filter").val();
-//         //   count = 0;
-  
-//         $('#results').each(function() {
-//             if ($('#results').text().search(new RegExp(filter, "i")) < 0) {
-//             $('#results').hide();  
-  
-//           } else {
-//             $('#results').show(); 
-//             // count++;
-//           }
-  
-//         });
-  
-//       });
-// })
+frappe.ready(function(){
+    $("#searchinput").keyup(function() {
+        let input = document.getElementById('searchinput').value
+        input = input.toLowerCase();
+        let x = document.getElementsByClassName('results');
+
+        for (i = 0; i < x.length; i++) {
+            if (!x[i].innerHTML.toLowerCase().includes(input)) {
+                x[i].style.display = "none";
+            }
+            else {
+                x[i].style.display = "";
+            }
+        }
+       
+    });
+
+    $("#selectiput").change(function(){
+        let input = document.getElementById('selectiput').value
+        input = input.toLowerCase();
+        let x = document.getElementsByClassName('results');
+
+        for (i = 0; i < x.length; i++) {
+            if (!x[i].innerHTML.toLowerCase().includes(input)) {
+                x[i].style.display = "none";
+            }
+            else {
+                x[i].style.display = "";
+            }
+        }        
+    });
+})
